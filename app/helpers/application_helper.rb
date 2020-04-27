ab931292f33293252ac92d51ca67f984d26d4bc0
Marco Calzada <mcalzada@kssoluciones.com>
2020-04-26 22:51:41 -0500

    Modiying layout view, for navigation bar and top bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,11 @@ module ApplicationHelper
     Employer.current_employer(user_id)
   end
 
+  def current_employer_name(user_id)
+    employer = self.current_employer(user_id)
+    employer.nil? ? 'Unknown User' : employer.first_name + employer.last_name
+  end
+
   def is_active_controller(controller_name)
     params[:controller] == controller_name ? 'active' : nil
   end
