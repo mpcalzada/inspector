93c1995cc7a3fc006e51cd838182500fe35dd501
Marco Calzada <mcalzada@kssoluciones.com>
2020-08-29 15:51:45 -0500

    Adding intial base for Accountancy Reports views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :accounts
-  resources :providers
-  resources :clients
+
   devise_for :users
 
   devise_scope :user do
@@ -11,36 +9,40 @@ Rails.application.routes.draw do
     get 'about' => 'landing#about'
     get 'home_landing' => 'landing#index'
 
+
+    resources :accounts
+    resources :providers
+    resources :clients
+    resources :loans
+    resources :departments
+    resources :attendance_tracker
+    resources :upload_files
+    resources :home
+    resources :employers
+    resources :degrees
+
+    get 'dashboard/home_dashboard'
+    get 'dashboard/accountancy_dashboard', as: :accountancy_dashboard
+    get 'dashboard/human_resources_dashboard'
+    get 'dashboard/reports_dashboard'
+
+    get 'reports/attendance_tracking', as: :attendance_report
+    get 'reports/accountancy_report', as: :accountancy_report
+
+    post 'attendance/historic/:id', to: 'attendance_tracker#employer_history'
+    post 'attendance/historic', to: 'attendance_tracker#full_history'
+
+    put 'employers/:id/attach_document', to: 'employers#attach_document'
+    put 'employers/:id/add_roles', to: 'employers#add_roles'
+    post 'employers/:id/enable_user', to: 'employers#enable_user'
+
+    post 'loans/prepayment', to: 'loans#prepayment'
+    post 'loans/:id/penalize', to: 'loans#penalize', as: :penalize_loan
+    post 'loans/:id/pay_monthly', to: 'loans#pay_monthly', as: :pay_monthly
+
+    put '/porcentual_filter_details/:id', to: 'porcentual_filter_details#create'
+
     authenticated :user do
-      resources :loans
-      resources :departments
-      resources :attendance_tracker
-      resources :upload_files
-      resources :home
-      resources :employers
-      resources :degrees
-
-      get 'dashboard/home_dashboard'
-      get 'dashboard/accountancy_dashboard', as: :accountancy_dashboard
-      get 'dashboard/human_resources_dashboard'
-      get 'dashboard/reports_dashboard'
-
-      get 'reports/attendance_tracking', as: :attendance_report
-
-
-      post 'attendance/historic/:id', to: 'attendance_tracker#employer_history'
-      post 'attendance/historic', to: 'attendance_tracker#full_history'
-
-      put 'employers/:id/attach_document', to: 'employers#attach_document'
-      put 'employers/:id/add_roles', to: 'employers#add_roles'
-      post 'employers/:id/enable_user', to: 'employers#enable_user'
-
-      post 'loans/prepayment', to: 'loans#prepayment'
-      post 'loans/:id/penalize', to: 'loans#penalize', as: :penalize_loan
-      post 'loans/:id/pay_monthly', to: 'loans#pay_monthly', as: :pay_monthly
-
-      put '/porcentual_filter_details/:id', to: 'porcentual_filter_details#create'
-
 
       root to: 'home#index', as: :authenticated_root
     end
