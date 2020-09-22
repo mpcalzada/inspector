8b53bf3195b329518b7988b58637cfd1cecd7597
Marco Calzada <arcocalzada@live.com.mx>
2020-09-21 22:14:35 -0500

    Adding real data into Home Dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
 
   def index
+    delayed_group = AttendanceOverview.grouped_by_month true
+    on_time_group = AttendanceOverview.grouped_by_month false
+    @searched_months = delayed_group.map { |k, v| k }
+    @delayed_punctuations = delayed_group.map { |k, v| v }
+    @on_time_punctuations = on_time_group.map { |k, v| v }
 
+    @active_loans_total = Loan.active.count
+    @inactive_loans_total = Loan.inactive.count
+    @total_loaned_amount = Loan.active.sum(:total_amount)
   end
 
 end
